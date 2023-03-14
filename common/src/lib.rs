1b6c4308be9eb443fd7317c541db6bc55329ae96
Alex Gonzalez <algonz@microsoft.com>
2023-03-15 10:16:39 +1100

    Added common types

---
+++ common/src/lib.rs
@@ -0,0 +1,161 @@
+pub mod types;
+pub use types::*; 
+
+use leptos::*;
+
+pub use reqwest::{
+    Response,
+    Error,
+};
+
+pub struct WebHelpers {}
+pub struct Loaders {}
+
+pub struct Actions {}
+
+impl WebHelpers {
+    pub async fn web_get(uri: &String) -> Result<Response, Error> {
+        let client = reqwest::Client::new();
+        match client.get(uri)
+            .send()
+            .await {
+            Ok(response) => {
+                log!("[reqwest] GET {:#?}...Ok", uri);
+                return Ok(response)
+            }
+            Err(error) => {
+                log!("[reqwest] GET {:#?}...Err", uri);
+                return Err(error)
+            }
+        };
+    }   
+
+    pub async fn web_post(uri: &String, json: &Value) -> Result<Response, Error> {
+        let client = reqwest::Client::new();
+        match client.post(uri)
+            .json(json)
+            .send()
+            .await {
+            Ok(response) => {
+                log!("[reqwest] POST {:#?}...Ok", uri);
+                return Ok(response)
+            }
+            Err(error) => {
+                log!("[reqwest] POST {:#?}...Err", uri);
+                return Err(error)
+            }
+        };
+    }   
+}
+
+impl Loaders {
+    pub async fn web_load_queue_message(url: String) -> Vec<Message> { 
+        let req_uri: String = format!("{}/api/nxfutil/status", url);
+        let req_body = r#"{
+                "summary": false,
+                "message_count": 32,
+                "dequeue": false
+            }"#;
+
+        let req_json: Value = serde_json::from_str(req_body).unwrap();
+        let res = WebHelpers::web_post(&req_uri, &req_json).await.unwrap();
+        let messages: Vec<Message> = res.json().await.unwrap();
+
+        return messages
+    }
+
+    pub async fn web_load_dispatcher_messages(dispatcher: NextflowDispatcher) -> Vec<Message> {
+        return Self::web_load_queue_message(dispatcher.url).await
+    }
+
+    pub async fn web_load_github_nextflow_workflow(project: NextflowProject) -> Vec<NextflowWorkflow> {
+        let res = WebHelpers::web_get(&project.url).await.unwrap();
+        let files: Vec<GitHubFile> = res.json().await.unwrap();
+
+        let mut nf_files: Vec<(String, String)> = vec![];
+        let mut json_files: Vec<(String, String)> = vec![];
+        for file in files {
+            if file.r#type == "file" {
+                if file.download_url.ends_with(".nf") {
+                    nf_files.push((file.name.clone(), file.download_url.clone()));
+                }
+                if file.download_url.ends_with(".json") {
+                    json_files.push((file.name.clone(), file.download_url.clone()));
+                }
+            }
+        }
+
+        let mut nextflow_workflows: Vec<NextflowWorkflow> = vec![];
+        for nf_file in &nf_files {
+            for json_file in &json_files {
+                nextflow_workflows.push(
+                    NextflowWorkflow {
+                        project: project.clone(),
+                        pipeline: NextflowFile {
+                            name: nf_file.0.clone(),
+                            url: nf_file.1.clone(),
+                        },
+                        parameters: NextflowFile {
+                            name: json_file.0.clone(),
+                            url:  json_file.1.clone(),
+                        }
+                    }
+                );
+            }
+        }
+
+        return nextflow_workflows
+    }
+
+    pub async fn web_load_github_nextflow_projects(org: String, repo: String) -> Vec<NextflowProject> {
+        let uri = format!("https://api.github.com/repos/{}/{}/contents/nextflow/pipelines", org, repo);
+        let res = WebHelpers::web_get(&uri).await.unwrap();
+        let dirs: Vec<GitHubDir> = res.json().await.unwrap();
+        
+        let mut nextflow_projects: Vec<NextflowProject> = vec![];
+        for dir in dirs {
+            if dir.r#type == "dir" {
+                nextflow_projects.push(
+                    NextflowProject {
+                        org: org.clone(),
+                        repo: repo.clone(),
+                        name: dir.name,
+                        url: dir.url,
+                        html_url: dir.html_url,
+                    }
+                );
+            }
+        }
+
+        return nextflow_projects
+    }    
+
+    pub async fn web_load_github_nextflow_workflows(repo: NextflowRepo) -> Vec<NextflowWorkflow> {
+        let mut projects: Vec<NextflowProject> = vec![];
+        projects.append(&mut Self::web_load_github_nextflow_projects(
+            repo.org, 
+            repo.name
+        ).await);
+    
+        let mut workflows: Vec<NextflowWorkflow> = vec![];
+        for project in projects {
+            workflows.append(&mut Self::web_load_github_nextflow_workflow(
+                project
+            ).await);
+        }
+    
+        return workflows
+    }  
+}
+
+
+impl Actions {
+    pub async fn web_action_dispatch_workflow(req: DispatchReq) -> DispatchRes {
+        let req_uri: String = String::from("http://localhost:7071/api/nxfutil/dispatch?whatif=true");
+        let req_json: Value = serde_json::to_value(req).unwrap();
+        let res = WebHelpers::web_post(&req_uri, &req_json).await.unwrap();
+        let result: DispatchRes = res.json().await.unwrap();
+
+        return result
+    }    
+}
---
+++ common/src/types.rs
@@ -0,0 +1,250 @@
+pub use serde::{
+    Deserialize, 
+    Serialize
+};
+
+pub use serde_json::{
+    Value,
+};
+
+pub use uuid::Uuid;
+
+/// Part of Message struct: message.metadata.workflow 
+#[allow(non_snake_case)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Workflow {
+    pub errorMessage: Option<String>
+}
+
+/// Part of Message struct: message.metadata 
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Metadata {
+    pub parameters: Value,
+    pub workflow: Workflow
+}
+
+/// Returned when azure-nextflow 'status' api finds a message
+#[allow(non_snake_case)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Message {
+    pub event: String,
+    pub runId: String,
+    pub runName: String,
+    pub utcTime: String,
+    pub metadata: Metadata,
+}
+
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+pub struct DispatchReqParam {
+    pub name: String,
+    pub value: Value,
+}
+
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+pub struct DispatchReq {
+    pub config_uri: String,
+    pub pipeline_uri: String,
+    pub parameters_uri: String,
+    pub parameters_json: Vec<DispatchReqParam>,
+    pub auto_delete: bool,
+}
+
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+pub struct DispatchRes {
+    pub sub_id: String,
+    pub rg_name: String,
+    pub ci_name: String,
+    pub ci_cmd: String,
+    pub provisioning_state: String,
+}
+
+/// Used in GitHubDir & GitHubFile struct: dir.type & file.type
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum GitHubFsType {
+    Dir,
+    File,
+}
+
+/// Returned when github 'contents' api finds a directory
+/// https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct GitHubDir {
+    pub r#type: String,
+    pub name: String,
+    pub path: String,
+    pub sha: String,
+    pub size: i32,
+    pub url: String,
+    pub html_url: String,
+    pub git_url: String,
+    pub download_url: Option<String>,
+} 
+
+/// Returned when github 'contents' api finds a file
+/// https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct GitHubFile {
+    pub r#type: String,
+    pub name: String,
+    pub path: String,
+    pub sha: String,
+    pub size: i32,
+    pub url: String,
+    pub html_url: String,
+    pub git_url: String,
+    pub download_url: String,
+}
+
+/// Part of NextflowWorkflow struct: workflow.project
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct NextflowProject {
+    pub org: String,
+    pub repo: String,
+    pub name: String,
+    pub url: String,
+    pub html_url: String,
+}
+
+/// Part of NextflowWorkflow struct: workflow.pipeline & workflow.parameters
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct NextflowFile {
+    pub name: String,
+    pub url: String,
+}
+
+/// Minified struct for rendering workflow
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct NextflowWorkflow {
+    pub project: NextflowProject,
+    pub pipeline: NextflowFile,
+    pub parameters: NextflowFile,
+}
+
+/// Minified struct for rendering repository
+#[derive(Debug, Clone)]
+pub struct NextflowRepo {
+    pub id: Uuid,
+    pub org: String,
+    pub name: String,
+}
+
+impl NextflowRepo {
+    pub fn new(id: Uuid, org: String, name: String) -> Self {
+        Self {
+            id,
+            org,
+            name,
+        }
+    }
+}
+
+/// Struct that works on repositories list as a shared signal
+#[derive(Debug, Clone)]
+pub struct NextflowRepos {
+    pub items: Vec<NextflowRepo>
+}
+
+impl NextflowRepos {
+    pub fn new() -> Self {
+        Self::mock_load()
+    }
+
+    pub fn mock_load() -> Self {
+        Self {
+            items: vec![
+                NextflowRepo::new(
+                    Uuid::new_v4(),
+                    "axgonz".to_string(),
+                    "azure-nextflow".to_string(),
+                )
+            ]
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+
+    pub fn add(&mut self, item: NextflowRepo) {
+        self.items.push(item)
+    }
+
+    pub fn remove(&mut self, id: Uuid) {
+        self.items.retain(|item| item.id != id)
+    }
+}
+
+/// Minified struct for rendering dispatcher
+#[derive(Debug, Clone)]
+pub struct NextflowDispatcher {
+    pub id: Uuid,
+    pub url: String,
+}
+
+impl NextflowDispatcher {
+    pub fn new(id: Uuid, url: String) -> Self {
+        Self {
+            id,
+            url,
+        }
+    }
+}
+
+/// Struct that works on dispatchers list as a shared signal
+#[derive(Debug, Clone)]
+pub struct NextflowDispatchers {
+    pub items: Vec<NextflowDispatcher>
+}
+
+impl NextflowDispatchers {
+    pub fn new() -> Self {
+        Self::mock_load()
+    }
+
+    pub fn mock_load() -> Self {
+        Self {
+            items: vec![
+                NextflowDispatcher::new(
+                    Uuid::new_v4(),
+                    "http://localhost:7071".to_string(),
+                )
+            ]
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+
+    pub fn url(&mut self) -> String {
+        if !self.items.is_empty() {
+            self.items[0].url.clone()
+        }
+        else {
+            "".to_string()
+        }
+    }
+
+    pub fn add(&mut self, item: NextflowDispatcher) {
+        self.items.push(item)
+    }
+
+    pub fn remove(&mut self, id: Uuid) {
+        self.items.retain(|item| item.id != id)
+    }
+}
+
+
+
+
+
+
+
+
+/// WARNING To be removed
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+pub struct Repo {
+    pub index: usize,
+    pub org: String,
+    pub name: String,
+}
